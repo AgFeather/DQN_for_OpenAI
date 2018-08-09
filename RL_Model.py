4d6aa6d1b68c165d38f8da0c6761b1413d19c88c
YHfeather <18843740508@163.com>
2018-08-09 23:41:05 +0900

    modify DQN model

--- RL_Model.py
+++ RL_Model.py
@@ -2,125 +2,13 @@
 import numpy as np
 import tensorflow as tf
 
+from SumTree import  SumTree
+from Memory import Memory
+
 np.random.seed(1)
 tf.set_random_seed(1)
 
 
-class SumTree(object):
-    '''
-    定义DQN中的Memory存储结构 SumTree
-    这里使用一个np.array表示整棵树
-    '''
-    data_pointer = 0
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.tree = np.zeros(2 * capacity - 1)
-        self.data = np.zeros(capacity, dtype=object)
-
-
-    def add(self, p, data):
-        '''
-        将新的记忆添加到tree中
-        :param p:
-        :param data:
-        :return:
-        '''
-        tree_idx = self.data_pointer + self.capacity - 1
-        self.data[self.data_pointer] = data
-        self.update(tree_idx, p)
-
-        self.data_pointer += 1
-        if self.data_pointer >= self.capacity:
-            self.data_pointer = 0
-
-    def update(self, tree_idx, p):
-        '''
-        当一个记忆数据被用来训练时，会产生新的优先值p，根据新的p值更新树
-        :param tree_idx:
-        :param p:
-        :return:
-        '''
-        change = p - self.tree[tree_idx]
-        self.tree[tree_idx] = p
-        while tree_idx != 0:
-            tree_idx = (tree_idx - 1) // 2
-            self.tree[tree_idx] += change
-
-    def get_leaf(self, v):
-        '''
-        根据选取的v进行抽样
-        :param v:
-        :return:
-        '''
-        parent_idx = 0
-        while True:
-            cl_idx = 2 * parent_idx + 1
-            cr_idx = cl_idx + 1
-            if cl_idx >= len(self.tree):
-                leaf_idx = parent_idx
-                break
-            else:
-                if v <= self.tree[cl_idx]:
-                    parent_idx = cl_idx
-                else:
-                    v -= self.tree[cl_idx]
-                    parent_idx = cr_idx
-
-        data_idx = leaf_idx - self.capacity + 1
-        return leaf_idx, self.tree[leaf_idx], self.data[data_idx]
-
-    @property
-    def total_p(self):
-        '''
-        获取全部优先级的和（根节点的值）
-        :return:
-        '''
-        return self.tree[0]
-
-
-class Memory(object):
-    '''
-    简历SumTree和各种参数
-    '''
-    epsilon = 0.01
-    alpha = 0.6
-    beta = 0.4
-    beta_increment_per_sampling = 0.001
-    abs_err_upper = 1.
-
-    def __init__(self, capacity):
-        self.tree = SumTree(capacity)
-
-    def store(self, transition):
-        '''存储数据，并且更新SumTree'''
-        max_p = np.max(self.tree.tree[-self.tree.capacity:])
-        if max_p == 0:
-            max_p = self.abs_err_upper
-        self.tree.add(max_p, transition)
-
-    def sample(self, n):
-        '''从SumTree中进行采样'''
-        b_idx, b_memory, ISWeights = np.empty((n,), dtype=np.int32), np.empty((n, self.tree.data[0].size)), np.empty((n, 1))
-        pri_seg = self.tree.total_p / n
-        self.beta = np.min([1., self.beta + self.beta_increment_per_sampling])
-
-        min_prob = np.min(self.tree.tree[-self.tree.capacity:]) / self.tree.total_p
-        for i in range(n):
-            a, b = pri_seg * i, pri_seg * (i + 1)
-            v = np.random.uniform(a, b)
-            idx, p, data = self.tree.get_leaf(v)
-            prob = p / self.tree.total_p
-            ISWeights[i, 0] = np.power(prob/min_prob, -self.beta)
-            b_idx[i], b_memory[i, :] = idx, data
-        return b_idx, b_memory, ISWeights
-
-    def batch_update(self, tree_idx, abs_errors):
-        '''对于一个记忆数据被训练完后，将新的数据更新到SumTree中'''
-        abs_errors += self.epsilon
-        clipped_errors = np.minimum(abs_errors, self.abs_err_upper)
-        ps = np.power(clipped_errors, self.alpha)
-        for ti, p in zip(tree_idx, ps):
-            self.tree.update(ti, p)
 
 
 class DQN(object):
